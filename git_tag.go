b92bffd645af9cf302ef295bc9f8e69589af8c1f
Ting-Rui, Wang <qwe50120@gmail.com>
2018-08-07 11:09:22 +0000

    fix bug when no tag

--- git_tag.go
+++ git_tag.go
@@ -33,15 +33,22 @@ func GetLatestTag() (string, error) {
 	output_str := strings.TrimSuffix(string(output), "\n")
 	tags := strings.Split(output_str, "\n")
 
-	if len(tags) == 0 {
-		return "0.0.0", errors.New("No exist tag, use 0.0.0")
+	var raw_tags []string
+	for _, tag := range tags {
+		if tag != "" {
+			raw_tags = append(raw_tags, tag)
+		}
 	}
 
 	var versions VersionList
-	for _, tag := range tags {
+	for _, tag := range raw_tags {
 		tag = strings.TrimPrefix(tag, "v")
 		v, _ := NewVersion(tag)
 		versions = append(versions, v)
+	}
+
+	if len(versions) == 0 {
+		return "0.0.0", errors.New("No exist tag, use 0.0.0")
 	}
 
 	sort.Sort(versions)
